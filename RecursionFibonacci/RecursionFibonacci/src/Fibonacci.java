4ab2ac9594c882231078668db4c157be64145531
G-Code13 <113222314+G-Code13@users.noreply.github.com>
2023-10-19 19:19:47 -0400

    update

---
+++ RecursionFibonacci/RecursionFibonacci/src/Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+    
+    Fibonacci(){
+
+    }
+
+    static int fib(int n){
+
+        if(n <= 1){
+            return n;
+        }
+        else{ 
+            return fib(n -1) + fib(n - 2);
+        }
+    }
+}

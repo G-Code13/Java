dc39e7334d17543c07a1b20e8f4a1a63f434ce5c
G-Code13 <113222314+G-Code13@users.noreply.github.com>
2023-11-11 19:22:52 -0500

    update

---
+++ Palindrome/Palidrome/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Palindrome palindrome = new Palindrome();
+        
+        System.out.println(palindrome.palindrome(121));
+    }
+}
---
+++ Palindrome/Palidrome/src/Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+    
+    boolean palindrome(int x){
+        if(x == 0){
+            return true;
+        }
+        if(x < 0 || x % 10 == 0){
+            return false;
+        }
+
+        int reverse = 0;
+        while (x > reverse) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return (x == reverse) || (x == reverse / 10);
+    }
+}
